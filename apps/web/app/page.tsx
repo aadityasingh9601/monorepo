b392ee9716f41698d5c6ac0877e94bdbcfcd6dfd
Aaditya Singh <aadityasingh9601@gmail.com>
2026-01-12 17:07:51 +0000

    small bug fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,6 @@
-//import { client } from "@repo/db/client";
+import { client } from "@repo/db/client";
 
 export default async function Home() {
-  //const user = await client.user.findFirst();
+  const user = await client.user.findFirst();
   return <div></div>;
 }
-
-// DATABASE_URL="postgresql://neondb_owner:npg_zDH9gTscklL8@ep-gentle-resonance-a1883sf5-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
