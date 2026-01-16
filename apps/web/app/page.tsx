e413297079769f7569929449a6879e8eed7c8d94
Aaditya Singh <aadityasingh9601@gmail.com>
2026-01-16 11:24:01 +0000

    testing cd_prod.yml

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div>
-      <h2>This is the production server.</h2>
+      <h2>This is the main production server.</h2>
       <div>This is the Username: {user?.username}</div>
       <div>This is the User_id: {user?.id}</div>
     </div>
