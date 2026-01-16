cd3e2c491ff21405d2441f951fe2ae0dcaa833a7
Aaditya Singh <aadityasingh9601@gmail.com>
2026-01-16 11:20:30 +0000

    testing cd_staging.yml

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,6 +4,7 @@ export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div>
+      <h2>This is the staging server.</h2>
       <div>This is the Username: {user?.username}</div>
       <div>This is the User_id: {user?.id}</div>
     </div>
